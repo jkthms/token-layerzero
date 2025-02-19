d663b028d69493bf23e7aa7ced21a3761b145d12
JakeXBT <jakeleediffusion@gmail.com>
2025-02-19 17:17:23 +0400

    chore: Refactor code to clean-up, `forge fmt`

--- script/Token.s.sol
+++ script/Token.s.sol
@@ -14,7 +14,7 @@ contract DeployToken is Script {
     // Base Sepolia: 84532
     address constant BASE_SEPOLIA_LZ_ENDPOINT = 0x6EDCE65403992e310A62460808c4b910D972f10f;
 
-    // Ethereum Mainnet: 1  
+    // Ethereum Mainnet: 1
     address constant ETHEREUM_MAINNET_LZ_ENDPOINT = 0x1a44076050125825900e736c501f859c50fE728c;
 
     // Ethereum Sepolia: 11155111
@@ -26,21 +26,27 @@ contract DeployToken is Script {
 
         string memory chain_pretty;
         address endpoint;
+        address treasury;
         if (chain == 1) {
             chain_pretty = "Ethereum Mainnet";
             endpoint = ETHEREUM_MAINNET_LZ_ENDPOINT;
+            treasury = address(0x0000000000000000000000000000000000000000);
         } else if (chain == 11155111) {
             chain_pretty = "Ethereum Sepolia";
             endpoint = SEPOLIA_LZ_ENDPOINT;
+            treasury = address(0x0000000000000000000000000000000000000000);
         } else if (chain == 8453) {
-            chain_pretty = "Base Mainnet"; 
+            chain_pretty = "Base Mainnet";
             endpoint = BASE_MAINNET_LZ_ENDPOINT;
+            treasury = address(0x0000000000000000000000000000000000000000);
         } else if (chain == 84532) {
             chain_pretty = "Base Sepolia";
             endpoint = BASE_SEPOLIA_LZ_ENDPOINT;
+            treasury = address(0x0000000000000000000000000000000000000000);
         } else if (chain == 998) {
             chain_pretty = "Hyperliquid Testnet";
             endpoint = HYPERLIQUID_TESTNET_LZ_ENDPOINT;
+            treasury = address(0x0000000000000000000000000000000000000000);
         } else {
             revert("REVERT: block.chainid was not handled...");
         }
@@ -48,10 +54,9 @@ contract DeployToken is Script {
         vm.startBroadcast();
         string memory name = "MockToken";
         string memory symbol = "MTOK";
-        address delegate = address(0x0000000000000000000000000000000000000000);
 
         // Deploy the MockERC20 contract with the LayerZero endpoint for this chain
-        Token token = new Token(name, symbol, endpoint, delegate);
+        Token token = new Token(name, symbol, endpoint, treasury);
         console.log("SUCCESS: Token deployed on %s to address: %s", chain_pretty, address(token));
         vm.stopBroadcast();
     }
